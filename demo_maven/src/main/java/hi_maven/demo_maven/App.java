a06e198db3e68c55ddd76fa9dedab1dffecc747e
unknown <nguyenthechau.ace@gmail.com>
2022-05-01 09:50:27 +0700

    abc

---
+++ demo_maven/src/main/java/hi_maven/demo_maven/App.java
@@ -0,0 +1,13 @@
+package hi_maven.demo_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
